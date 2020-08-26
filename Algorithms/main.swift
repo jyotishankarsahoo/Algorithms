c816ec61f33927a466e7d1fb32cb16ab8f017399
jyoti shankar sahoo <jyotishankarsahoo86@gmail.com>
2020-08-26 10:14:25 -0500

    Initial Commit

---
+++ Algorithms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Algorithms
+//
+//  Created by Jyoti Sahoo on 8/26/20.
+//  Copyright © 2020 Jyoti Sahoo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
